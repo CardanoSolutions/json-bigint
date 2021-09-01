391ebb10dda57a8ce133bc74d30cbe52a71ed196
KtorZ <matthias.benkort@gmail.com>
2021-09-01 18:11:43 +0200

    Add test to illustrate parsing issue with legit decimals.

--- test/bigint-parse-test.js
+++ test/bigint-parse-test.js
@@ -1,44 +1,41 @@
-var mocha = require('mocha')
-  , assert = require('chai').assert
-  , expect = require('chai').expect
-  , BigNumber = require('bignumber.js')
-  ;
-
-describe("Testing native BigInt support: parse", function () {
-  if (typeof (BigInt) === 'undefined') {
+var mocha = require('mocha'),
+  assert = require('chai').assert,
+  expect = require('chai').expect,
+  BigNumber = require('bignumber.js');
+describe('Testing native BigInt support: parse', function () {
+  if (typeof BigInt === 'undefined') {
     console.log('No native BigInt');
     return;
   }
   var input = '{"big":92233720368547758070,"small":123}';
 
-  it("Should show JSONbig does support parsing native BigInt", function (done) {
+  it('Should show JSONbig does support parsing native BigInt', function (done) {
     var JSONbig = require('../index')({
-      "useNativeBigInt": true
+      useNativeBigInt: true,
     });
     var obj = JSONbig.parse(input);
-    expect(obj.small, "small int").to.equal(123);
-    expect(obj.big.toString(), "big int").to.equal("92233720368547758070");
-    expect(typeof obj.big, "big int").to.equal('bigint');
+    expect(obj.small, 'small int').to.equal(123);
+    expect(obj.big.toString(), 'big int').to.equal('92233720368547758070');
+    expect(typeof obj.big, 'big int').to.equal('bigint');
     done();
   });
 
-  it("Should show JSONbig does support forced parsing to native BigInt", function (done) {
+  it('Should show JSONbig does support forced parsing to native BigInt', function (done) {
     var JSONbig = require('../index')({
-      "alwaysParseAsBig": true,
-      "useNativeBigInt": true
+      alwaysParseAsBig: true,
+      useNativeBigInt: true,
     });
     var obj = JSONbig.parse(input);
-    expect(obj.big.toString(), "big int").to.equal("92233720368547758070");
-    expect(typeof obj.big, "big int").to.equal('bigint');
-    expect(obj.small.toString(), "small int").to.equal("123");
-    expect(typeof obj.small, "small int").to.equal('bigint');
+    expect(obj.big.toString(), 'big int').to.equal('92233720368547758070');
+    expect(typeof obj.big, 'big int').to.equal('bigint');
+    expect(obj.small.toString(), 'small int').to.equal('123');
+    expect(typeof obj.small, 'small int').to.equal('bigint');
     done();
   });
 
-
-  it("Should show JSONbig does support native Bigint parse/stringify roundtrip", function (done) {
+  it('Should show JSONbig does support native Bigint parse/stringify roundtrip', function (done) {
     var JSONbig = require('../index')({
-      "useNativeBigInt": true
+      useNativeBigInt: true,
     });
     var obj = JSONbig.parse(input);
     var output = JSONbig.stringify(obj);
@@ -46,14 +43,21 @@ describe("Testing native BigInt support: parse", function () {
     done();
   });
 
-  it("Should show JSONbig does support native Bigint parse/stringify roundtrip when BigInt is forced", function (done) {
+  it('Should show JSONbig does support native Bigint parse/stringify roundtrip when BigInt is forced', function (done) {
     var JSONbig = require('../index')({
-      "alwaysParseAsBig": true,
-      "useNativeBigInt": true
+      alwaysParseAsBig: true,
+      useNativeBigInt: true,
     });
     var obj = JSONbig.parse(input);
     var output = JSONbig.stringify(obj);
     expect(output).to.equal(input);
+    done();
+  });
+
+  it('Should not attempt to parse decimal numbers as BigInt.', function (done) {
+    var JSONbig = require('../index')({ useNativeBigInt: true });
+    var result = JSONbig.parse('5.469120721016755e9');
+    expect(result).to.equal(5.469120721016755e9);
     done();
   });
 });
